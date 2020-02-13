ef8c3543d2097f860dd832c8f910a24face19458
Abubaker Noorzi <anoorzi123@gmail.com>
2020-02-13 05:55:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name="g")
-  print " Hi, my name is #{name}.\n "
+  print " Hi, my naame is #{name}.\n "
 end
 
 
