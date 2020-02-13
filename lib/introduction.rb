17a89b76aef67126a6cca4d8458b0baab737a3a4
Abubaker Noorzi <anoorzi123@gmail.com>
2020-02-13 05:59:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,9 @@ def introduction(name="g")
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, lang)
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
